03542ba47161601f22c63f0d16b5e49778bdd64c
juhavuo <juha.vuokko@pp.inet.fi>
2023-08-11 15:09:48 +0300

    added SettingsActivity

--- app/src/main/java/fi/julavu/cameramovements/SettingsActivity.kt
+++ app/src/main/java/fi/julavu/cameramovements/SettingsActivity.kt
@@ -1,8 +1,6 @@
 package fi.julavu.cameramovements
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,17 +12,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import fi.julavu.cameramovements.ui.theme.CameraMovementsTheme
 
-class MainActivity : ComponentActivity() {
+class SettingsActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val settingsButton = findViewById<Button>(R.id.main_activity_settings_button)
-        settingsButton.setOnClickListener {
-            val intent = Intent(this, SettingsActivity::class.java)
-            startActivity(intent)
-        }
+        setContentView(R.layout.activity_settings)
     }
-
-
 }
+
