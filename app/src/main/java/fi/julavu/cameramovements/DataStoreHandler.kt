7c1befdc926df77a213ac6342dc9c1a2ded54fcc
juhavuo <juha.vuokko@pp.inet.fi>
2023-08-17 12:45:46 +0300

    before updating gradle imports

--- app/src/main/java/fi/julavu/cameramovements/DataStoreHandler.kt
+++ app/src/main/java/fi/julavu/cameramovements/DataStoreHandler.kt
@@ -1,4 +1,16 @@
 package fi.julavu.cameramovements
 
-class DataStoreHandler {
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+
+
+
+class DataStoreHandler(context: Context) {
+
+    /*
+    private val Context.dataStore = DataStore<Preferences> by preferencesDataStore(name = "settings")
+    private val dataStore = context.dataStore*/
+
 }
