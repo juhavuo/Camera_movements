1ffd07610ac4b5320a2f03934fdf40e6c1e06c2b
juhavuo <juha.vuokko@pp.inet.fi>
2023-09-13 21:50:05 +0300

    saving seekbar settings works again

--- app/src/main/java/fi/julavu/cameramovements/SettingsActivity.kt
+++ app/src/main/java/fi/julavu/cameramovements/SettingsActivity.kt
@@ -6,6 +6,7 @@ import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.RelativeLayout
 import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import android.widget.Spinner
 import android.widget.TextView
 import androidx.activity.ComponentActivity
@@ -19,6 +20,9 @@ import kotlinx.coroutines.launch
 class SettingsActivity : ComponentActivity() {
 
     private lateinit var dataStoreHandler: DataStoreHandler
+    private lateinit var durationSeekBar: SeekBar
+    private val settingsDataList = ArrayList<SettingsData>()
+    private val seekBars = ArrayList<SeekBar>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
@@ -35,14 +39,51 @@ class SettingsActivity : ComponentActivity() {
         sizesSpinner.adapter = sizesSpinnerAdapter
 
         val durationSettings = SettingsData.getSettingsData(this,R.string.for_duration_seekbar)
-        val durationSeekBar = getAndSetupSeekbar(R.id.settings_activity_duration_view,durationSettings)
+        setupSeekbar(R.id.settings_activity_duration_view,durationSettings)
         CoroutineScope(Dispatchers.Main).launch {
-            durationSeekBar.progress = dataStoreHandler.getSeekbarProgressValue(durationSettings)
+          for(i in 0 until seekBars.size){
+              settingsDataList[i].progress = dataStoreHandler.getSeekbarProgressValue(settingsDataList[i])
+              seekBars[i].progress = settingsDataList[i].progress
+          }
+        }
+        for(i in 0 until seekBars.size){
+            seekBars[i].setOnSeekBarChangeListener(object: OnSeekBarChangeListener{
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    settingsDataList[i].progress = progress
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+
+            })
         }
 
+        /*
+
+        val durationSettings = SettingsData.getSettingsData(this,R.string.for_duration_seekbar)
+        durationSeekBar = getAndSetupSeekbar(R.id.settings_activity_duration_view,durationSettings)
+        CoroutineScope(Dispatchers.Main).launch {
+            durationSettings.progress = dataStoreHandler.getSeekbarProgressValue(durationSettings)
+            durationSeekBar.progress = durationSettings.progress
+            settingsDataList.add(durationSettings)
+        }
+
+        durationSeekBar.setOnSeekBarChangeListener(object: OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                settingsDataList[settingsDataList.indexOfFirst { it.tag == "duration" }].progress = progress
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+        */
         //val settingsDataList = SettingsData.getDefaultSettingsDataList(this)
-
-
         /*
         val settingsRecyclerView = findViewById<RecyclerView>(R.id.settings_activity_recyclerview)
         val linearLayoutManager = LinearLayoutManager(this)
@@ -54,38 +95,43 @@ class SettingsActivity : ComponentActivity() {
         var startValueForSeekbar = 0
         val dataStoreHandler = DataStoreHandler(this)
 
-        Log.i("cameramovements_testing","$startValueForSeekbar")
-
-
-
-        //Log.i("cameramovements_testing","${timespanSeekbar.min}")
+*/
 
         val saveButton = findViewById<Button>(R.id.settings_activity_save_and_quit_button)
         saveButton.setOnClickListener {
-            settingsAdapter.saveSeekbarProgressesToDataStore()
+            saveAllSettings()
             goBackToRecording()
         }
 
         val cancelButton = findViewById<Button>(R.id.settings_activity_cancel_button)
         cancelButton.setOnClickListener {
             goBackToRecording()
-        }*/
+        }
     }
 
-    private fun getAndSetupSeekbar(id: Int, settingsData: SettingsData): SeekBar{
+    private fun setupSeekbar(id: Int, settingsData: SettingsData){
 
         val seekBarLayout = findViewById<RelativeLayout>(id)
         val seekBar = seekBarLayout.findViewById<SeekBar>(R.id.template_seekbar)
+        val seekBarTitle = seekBarLayout.findViewById<TextView>(R.id.seekbar_title)
+        seekBarTitle.text = settingsData.title
         val seekBarMinTextView = seekBarLayout.findViewById<TextView>(R.id.seekbar_min_text_view)
         seekBarMinTextView.text = settingsData.sliderMin.toString()
         val seekBarMaxTextView = seekBarLayout.findViewById<TextView>(R.id.seekbar_max_text_view)
         seekBarMaxTextView.text = settingsData.sliderMax.toString()
-        return seekBar
+        seekBars.add(seekBar)
+        settingsDataList.add(settingsData)
     }
 
     private fun goBackToRecording(){
         val intent = Intent(this,RecordingActivity::class.java)
         startActivity(intent)
+    }
+
+    private fun saveAllSettings(){
+        CoroutineScope(Dispatchers.Main).launch {
+            dataStoreHandler.writeSeekbarProgressValues(settingsDataList)
+        }
     }
 }
 
