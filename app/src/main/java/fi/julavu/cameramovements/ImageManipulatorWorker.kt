d96d5ef9829c5afcf7d50348704dd06bc289a7bc
juhavuo <juha.vuokko@pp.inet.fi>
2023-11-28 16:53:29 +0200

    version 2 with just bitmaps, no bitmapdrawable

--- app/src/main/java/fi/julavu/cameramovements/ImageManipulatorWorker.kt
+++ app/src/main/java/fi/julavu/cameramovements/ImageManipulatorWorker.kt
@@ -14,6 +14,7 @@ package fi.julavu.cameramovements
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Color
+import android.graphics.drawable.BitmapDrawable
 import android.util.Log
 import androidx.core.graphics.set
 import androidx.work.Worker
@@ -61,11 +62,19 @@ class ImageManipulatorWorker(c: Context, workerParameters: WorkerParameters): Wo
                 bitmap = fileHandler.getBitmap(files!![0])
                 if(bitmap != null) {
                     createEmptyBitmap(bitmap)
-                    addBitmapToBase(bitmap,pixelSize)
+                    if(pixelSize == 1) {
+                        addBitmapToBase(bitmap)
+                    }else{
+                        addBitmapToBase(bitmap,pixelSize)
+                    }
                     for (i in 1 until files!!.size) {
                         bitmap = fileHandler.getBitmap(files!![i])
                         if(bitmap != null) {
-                            addBitmapToBase(bitmap,pixelSize)
+                            if(pixelSize == 1) {
+                                addBitmapToBase(bitmap)
+                            }else{
+                                addBitmapToBase(bitmap,pixelSize)
+                            }
                         }
                     }
                 }
@@ -95,20 +104,42 @@ class ImageManipulatorWorker(c: Context, workerParameters: WorkerParameters): Wo
         bitmapBase = Bitmap.createBitmap(width,height,conf)
     }
 
+    private fun addBitmapToBase(bm: Bitmap){
+        if(bitmapBase != null){
+            var r: Int
+            var g: Int
+            var b: Int
+            var pixel: Int
+            var pixelBM: Int
+
+            for(x in 0 until bitmapBase!!.width){
+                for(y in 0 until bitmapBase!!.height){
+                    pixel = bitmapBase!!.getPixel(x, y)
+                    pixelBM = bm.getPixel(x,y)
+                    r = Color.red(pixel)+Color.red(pixelBM)/amountOfFiles
+                    g = Color.green(pixel)+Color.green(pixelBM)/amountOfFiles
+                    b = Color.blue(pixel)+Color.blue(pixelBM)/amountOfFiles
+
+                    bitmapBase!![x, y] = Color.rgb(r,g,b)
+
+                }//for(y...
+            }//for(x...)
+        }//if(bitmapBase != null)
+    }//fun addBitmapToBase
+
+    /*
+     * add to bitmapbase when pixel size is set to other than 1. Now color is taken in
+     * upper left corner of area and adding color to whole region with width and height
+     * both pixelSize is unefficient. If there is a good way to set multiple of pixels
+     * in bitmap at once, will move to that when found.
+     */
     private fun addBitmapToBase(bm: Bitmap, pixelSize: Int){
         if(bitmapBase != null){
             var r: Int
             var g: Int
             var b: Int
             var pixel: Int
-            var pixel2: Int
-            var pixel3: Int
-            var pixel4: Int
             var pixelBM: Int
-            var pixelBM2: Int
-            var pixelBM3: Int
-            var pixelBM4: Int
-
             for(x in 0 until bitmapBase!!.width-pixelSize step pixelSize){
                 for(y in 0 until bitmapBase!!.height-pixelSize step pixelSize){
                     pixel = bitmapBase!!.getPixel(x, y)
@@ -116,34 +147,52 @@ class ImageManipulatorWorker(c: Context, workerParameters: WorkerParameters): Wo
                     r = Color.red(pixel)+Color.red(pixelBM)/amountOfFiles
                     g = Color.green(pixel)+Color.green(pixelBM)/amountOfFiles
                     b = Color.blue(pixel)+Color.blue(pixelBM)/amountOfFiles
-                    /*
-                    if(pixelSize < 30) {
-                        pixel = bitmapBase!!.getPixel(x, y)
-                        pixelBM = bm.getPixel(x,y)
-                        r = Color.red(pixel)+Color.red(pixelBM)/amountOfFiles
-                        g = Color.green(pixel)+Color.green(pixelBM)/amountOfFiles
-                        b = Color.blue(pixel)+Color.blue(pixelBM)/amountOfFiles
-                    }else{
-                        pixel = bitmapBase!!.getPixel(x, y)
-                        pixel2 = bitmapBase!!.getPixel(x+pixelSize-1, y)
-                        pixel3 = bitmapBase!!.getPixel(x, y+pixelSize-1)
-                        pixel4 = bitmapBase!!.getPixel(x+pixelSize-1, y+pixelSize-1)
-                        pixelBM= bm.getPixel(x,y)
-                        pixelBM2=bm.getPixel(x+pixelSize-1,y)
-                        pixelBM3=bm.getPixel(x,y+pixelSize-1)
-                        pixelBM4=bm.getPixel(x+pixelSize-1,y+pixelSize-1)
-                        Log.i(MyApplication.tagForTesting,"p1: $pixel p2: $pixel2 p3 $pixel3 p4 $pixel4")
-                        r = (Color.red(pixel)+Color.red(pixel2)+Color.red(pixel3)+Color.red(pixel4))/4
-                        +(Color.red(pixelBM)+Color.red(pixelBM2)+Color.red(pixelBM3)+Color.red(pixelBM4))/(4*amountOfFiles)
-                        g = (Color.green(pixel)+Color.green(pixel2)+Color.green(pixel3)+Color.green(pixel4))/4
-                        +(Color.green(pixelBM)+Color.green(pixelBM2)+Color.green(pixelBM3)+Color.green(pixelBM4))/(4*amountOfFiles)
-                        b = (Color.blue(pixel)+Color.blue(pixel2)+Color.blue(pixel3)+Color.blue(pixel4))/4
-                        +(Color.blue(pixelBM)+Color.blue(pixelBM2)+Color.blue(pixelBM3)+Color.blue(pixelBM4))/(4*amountOfFiles)
-                    }*/
-                    bitmapBase!![x, y] = Color.rgb(r,g,b)
+                   //not very efficient solution
+                    for(i in 0 until pixelSize) {
+                        for(j in 0 until pixelSize) {
+                            bitmapBase!![x+i, y+j] = Color.rgb(r, g, b)
+                        }
+                    }
 
                 }//for(y...
             }//for(x...)
         }//if(bitmapBase != null)
     }//fun addBitmapToBase
 }
+
+
+
+
+/*
+
+            var pixel2: Int
+            var pixel3: Int
+            var pixel4: Int
+
+            var pixelBM2: Int
+            var pixelBM3: Int
+            var pixelBM4: Int
+
+                   if(pixelSize < 30) {
+                       pixel = bitmapBase!!.getPixel(x, y)
+                       pixelBM = bm.getPixel(x,y)
+                       r = Color.red(pixel)+Color.red(pixelBM)/amountOfFiles
+                       g = Color.green(pixel)+Color.green(pixelBM)/amountOfFiles
+                       b = Color.blue(pixel)+Color.blue(pixelBM)/amountOfFiles
+                   }else{
+                       pixel = bitmapBase!!.getPixel(x, y)
+                       pixel2 = bitmapBase!!.getPixel(x+pixelSize-1, y)
+                       pixel3 = bitmapBase!!.getPixel(x, y+pixelSize-1)
+                       pixel4 = bitmapBase!!.getPixel(x+pixelSize-1, y+pixelSize-1)
+                       pixelBM= bm.getPixel(x,y)
+                       pixelBM2=bm.getPixel(x+pixelSize-1,y)
+                       pixelBM3=bm.getPixel(x,y+pixelSize-1)
+                       pixelBM4=bm.getPixel(x+pixelSize-1,y+pixelSize-1)
+                       Log.i(MyApplication.tagForTesting,"p1: $pixel p2: $pixel2 p3 $pixel3 p4 $pixel4")
+                       r = (Color.red(pixel)+Color.red(pixel2)+Color.red(pixel3)+Color.red(pixel4))/4
+                       +(Color.red(pixelBM)+Color.red(pixelBM2)+Color.red(pixelBM3)+Color.red(pixelBM4))/(4*amountOfFiles)
+                       g = (Color.green(pixel)+Color.green(pixel2)+Color.green(pixel3)+Color.green(pixel4))/4
+                       +(Color.green(pixelBM)+Color.green(pixelBM2)+Color.green(pixelBM3)+Color.green(pixelBM4))/(4*amountOfFiles)
+                       b = (Color.blue(pixel)+Color.blue(pixel2)+Color.blue(pixel3)+Color.blue(pixel4))/4
+                       +(Color.blue(pixelBM)+Color.blue(pixelBM2)+Color.blue(pixelBM3)+Color.blue(pixelBM4))/(4*amountOfFiles)
+                   }*/
